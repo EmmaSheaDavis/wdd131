d0c5331597f2de1d1521955ca53f48ba6f536048
EmmaSheaDavis <emmadavis@byui.edu>
2025-01-15 09:35:17 -0700

    explanitory comments

--- week2/dom-basics.js
+++ week2/dom-basics.js
@@ -1,12 +1,15 @@
+//create paragraph
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
 
+//add image
 const newImage = document.createElement("img");
 newImage.setAttribute("src", "https://picsum.photos/200");
 newImage.setAttribute("alt", "Random image from picsum.");
 document.body.appendChild(newImage);
 
+//add section, h2, and p
 const newSection = document.createElement("section");
 const newH2 = document.createElement("h2");
 newH2.innerText = "DOM basics";
@@ -17,7 +20,7 @@ newSection.appendChild(newP);
 
 document.body.appendChild(newSection);
 
-//or could also be done like this: 
-//const newSection = document.createElement("section");
-//newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
-//document.body.appendChild(newSection);
+//or they could also be added like this: 
+  //const newSection = document.createElement("section");
+  //newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+  //document.body.appendChild(newSection);
